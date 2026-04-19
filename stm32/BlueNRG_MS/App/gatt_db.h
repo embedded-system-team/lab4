dc0d6206d88434b3a13023377bcfaa2f6a7311b1
gimips <cnick7364@gmail.com>
2026-04-20 00:33:36 +0800

    feat(ble): implement custom Accelerometer GATT service with Char_A and Char_B

--- stm32/BlueNRG_MS/App/gatt_db.h
+++ stm32/BlueNRG_MS/App/gatt_db.h
@@ -1,84 +1,73 @@
 /**
   ******************************************************************************
   * @file    App/gatt_db.h
-  * @author  SRA Application Team
-  * @brief   Header file for App/gatt_db.c
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
+  * @brief   Custom Accelerometer GATT Service declarations
   ******************************************************************************
   */
 
 #ifndef GATT_DB_H
 #define GATT_DB_H
 
-#include <stdint.h>
-#include <stdlib.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "bluenrg_def.h"
+#include "bluenrg_gatt_aci.h"
 
-#define X_OFFSET 200
-#define Y_OFFSET 50
-#define Z_OFFSET 1000
-
-/**
- * @brief Number of application services
- */
-#define NUMBER_OF_APPLICATION_SERVICES (2)
+/* Exported variables --------------------------------------------------------*/
+extern uint16_t AccServiceHandle;
+extern uint16_t AccDataCharHandle;   /* Char_A: Accel Data */
+extern uint16_t AccFreqCharHandle;   /* Char_B: Sampling Frequency */
 
 /**
- * @brief Define How Many quaterions you want to transmit (from 1 to 3)
- *        In this sample application use only 1
+ * @brief ODR enum index definition
+ *
+ * RPi writes one of these values to Char_B.
+ * STM32 maps the index to the corresponding LSM6DSL ODR setting
+ * and RTOS task delay period.
+ *
+ * Index | ODR      | Period
+ * ------|----------|---------
+ *   0   | 12.5 Hz  |  80 ms
+ *   1   |  26 Hz   |  38 ms
+ *   2   |  52 Hz   |  19 ms
+ *   3   | 104 Hz   |   9 ms
  */
-#define SEND_N_QUATERNIONS 1
+typedef enum {
+  ACC_ODR_12HZ5 = 0,
+  ACC_ODR_26HZ  = 1,
+  ACC_ODR_52HZ  = 2,
+  ACC_ODR_104HZ = 3,
+  ACC_ODR_MAX   = 3
+} AccODR_Index_t;
+
+/* Exported functions --------------------------------------------------------*/
 
 /**
- * @brief Structure containing acceleration value of each axis.
+ * @brief  Add the custom Accelerometer GATT service (Char_A + Char_B).
+ * @retval BLE_STATUS_SUCCESS or BLE_STATUS_ERROR
  */
-typedef struct {
-  int32_t AXIS_X;
-  int32_t AXIS_Y;
-  int32_t AXIS_Z;
-} AxesRaw_t;
+tBleStatus Add_AccService(void);
 
-enum {
-  ACCELERATION_SERVICE_INDEX = 0,
-  ENVIRONMENTAL_SERVICE_INDEX = 1
-};
+/**
+ * @brief  Update Char_A value and send BLE notification to subscribed client.
+ * @param  x  X-axis in mg
+ * @param  y  Y-axis in mg
+ * @param  z  Z-axis in mg
+ * @retval BLE_STATUS_SUCCESS or BLE_STATUS_ERROR
+ */
+tBleStatus AccData_Update(int16_t x, int16_t y, int16_t z);
 
-/** Documentation for C union Service_UUID_t */
-typedef union Service_UUID_t_s {
-  /** 16-bit UUID
-  */
-  uint16_t Service_UUID_16;
-  /** 128-bit UUID
-  */
-  uint8_t Service_UUID_128[16];
-} Service_UUID_t;
+/**
+ * @brief  Handle READ request on Char_A.
+ * @param  conn_handle  Current BLE connection handle
+ * @param  x, y, z      Latest acceleration values
+ */
+void AccData_ReadRequestCB(uint16_t conn_handle, int16_t x, int16_t y, int16_t z);
 
-/** Documentation for C union Char_UUID_t */
-typedef union Char_UUID_t_s {
-  /** 16-bit UUID
-  */
-  uint16_t Char_UUID_16;
-  /** 128-bit UUID
-  */
-  uint8_t Char_UUID_128[16];
-} Char_UUID_t;
-
-tBleStatus Add_HWServW2ST_Service(void);
-tBleStatus Add_SWServW2ST_Service(void);
-void Read_Request_CB(uint16_t handle);
-tBleStatus BlueMS_Environmental_Update(int32_t press, int16_t temp);
-tBleStatus Acc_Update(AxesRaw_t *x_axes, AxesRaw_t *g_axes, AxesRaw_t *m_axes);
-tBleStatus Quat_Update(AxesRaw_t *q_axes);
-
-extern uint8_t Services_Max_Attribute_Records[];
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* GATT_DB_H */
